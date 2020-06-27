c34149c9c2c45e23b1f012fd1e66fadf2c7f6303
Zion Sandeep <59429929+zion-s@users.noreply.github.com>
2020-06-27 22:58:20 +0530

    added sorting functionality to results

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -17,14 +17,29 @@ let business = {
 
 let businesses = [business, business, business, business, business, business];
 
-function App() {
-  return (
-    <div className="App">
-  <h1>ravenous</h1>
-  <SearchBar />
-  <BusinessList businesses={businesses}/> 
-</div>
-  );
+class App extends React.Component{
+  searchYelp(term,location,sortBy){
+    console.log(`Searching Yelp with ${term}, ${location}, ${sortBy}`);
+  }
+  render(){
+    return (
+      <div className="App">
+    <h1>ravenous</h1>
+    <SearchBar searchYelp={this.searchYelp}/>
+    <BusinessList businesses={businesses}/> 
+  </div>
+    );
+  }
 }
+
+// function App() {
+//   return (
+//     <div className="App">
+//   <h1>ravenous</h1>
+//   <SearchBar />
+//   <BusinessList businesses={businesses}/> 
+// </div>
+//   );
+// }
 
 export default App;
