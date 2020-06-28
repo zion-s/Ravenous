00df4959a096d209e2d8a0b1cce2cb54c35c56ed
Zion Sandeep <59429929+zion-s@users.noreply.github.com>
2020-06-29 00:19:09 +0530

    Debugging
    added binding 'this' of searchYelp

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,40 +1,37 @@
 import React from 'react';
 import './App.css';
-import BusinessList from './../BusinessList/BusinessList';
-import SearchBar from './../SearchBar/SearchBar';
-import Yelp from './../../util/Yelp';
 
-class App extends React.Component{
-  constructor(props){
-    this.state={
-      businesses:[]
-    }
-    this.searchYelp.bind(this);
+import BusinessList from '../BusinessList/BusinessList';
+import SearchBar from '../SearchBar/SearchBar';
+
+import Yelp from '../../util/Yelp';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      businesses: []
+    };
+
+    this.searchYelp = this.searchYelp.bind(this);
   }
-  searchYelp(term,location,sortBy){
-    Yelp.search(term,location,sortBy).then(businesses=>{
-      this.setState({businesses:businesses})
+
+  searchYelp(term, location, sortBy) {
+    Yelp.search(term, location, sortBy).then(businesses => {
+      this.setState({businesses: businesses});
     });
   }
-  render(){
+
+  render() {
     return (
       <div className="App">
-    <h1>ravenous</h1>
-    <SearchBar searchYelp={this.searchYelp}/>
-    <BusinessList businesses={businesses}/> 
-  </div>
+        <h1>ravenous</h1>
+        <SearchBar searchYelp={this.searchYelp} />
+        <BusinessList businesses={this.state.businesses} />
+      </div>
     );
   }
 }
-
-// function App() {
-//   return (
-//     <div className="App">
-//   <h1>ravenous</h1>
-//   <SearchBar />
-//   <BusinessList businesses={businesses}/> 
-// </div>
-//   );
-// }
 
 export default App;
